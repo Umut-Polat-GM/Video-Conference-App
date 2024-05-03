780ceec5aa7171686afe1dea3c71986ac5e5372c
Umut-Polat-GM <umut.polat.272@gmail.com>
2024-05-04 00:56:31 +0300

    chore: Add Clerk.js authentication for sign-in and sign-up pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Sign } from "crypto";
 
 const Navbar = () => {
     return (
@@ -17,6 +19,9 @@ const Navbar = () => {
                 <p className='"text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
             </Link>
             <div className="flex-between gap-5">
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
                 <MobileNav />
             </div>
         </nav>
